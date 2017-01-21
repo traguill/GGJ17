8ea2e127178a46bebd6faacb33fb0b14163b0853
J-Nunes <jordinunes96@gmail.com>
2017-01-21 19:39:20 +0100

    Improving UI when you lose

--- GGJ17/Assets/Code/UI/GameLoop.cs
+++ GGJ17/Assets/Code/UI/GameLoop.cs
@@ -6,11 +6,18 @@ using System.Collections;
 public class GameLoop : MonoBehaviour 
 {
     public static GameLoop manager;
+
     public Image life1;
     public Image life2;
     public Image life3;
-    public int lifes = 3;
 
+    public Text you_lose;
+    Color you_lose_alpha;
+
+    public Text press_button;
+    Color press_button_alpha;
+
+    public int lifes = 3;
     bool game_over = false;
 
     void Awake()
@@ -23,6 +30,14 @@ public class GameLoop : MonoBehaviour
         life1.gameObject.SetActive(true);
         life2.gameObject.SetActive(true);
         life3.gameObject.SetActive(true);
+
+        you_lose_alpha = you_lose.color;
+        you_lose_alpha.a = 0;
+        you_lose.color = you_lose_alpha;
+
+        press_button_alpha = press_button.color;
+        press_button_alpha.a = 0;
+        press_button.color = press_button_alpha;
     }
 
     // Update is called once per frame
@@ -30,9 +45,24 @@ public class GameLoop : MonoBehaviour
     {
 	    if(game_over)
         {
-            SceneManager.LoadScene("MenuScene");
+            if (you_lose_alpha.a <= 1.0f)
+            {
+                you_lose_alpha.a += 0.01f;
+                you_lose.color = you_lose_alpha;
+            }
+
+            else if(press_button_alpha.a <= 1.0f)
+            {
+                press_button_alpha.a += 0.01f;
+                press_button.color = press_button_alpha;
+            }
+
+            else if(Input.anyKey)
+            {
+                SceneManager.LoadScene("MenuScene");
+            }
         }
-	}
+    }
 
     public void RemoveLife()
     {
