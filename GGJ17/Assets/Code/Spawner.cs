6e9983e53d4f51fec823c97da6a6c78c06c71a21
J-Nunes <jordinunes96@gmail.com>
2017-01-21 01:12:57 +0100

    Spawn System

---
+++ GGJ17/Assets/Code/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        //CreateEnemy();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void CreateEnemy()
+    {
+        GameObject obj = GameObject.Find("Enemy");
+        Instantiate(obj, transform.position, transform.rotation);
+    }
+}
