eadb094093dfdd46e656fb4ce0ad5e3738ad1c14
traguill <traguill1@gmail.com>
2017-01-21 12:47:07 +0100

    Sorting sprites

---
+++ GGJ17/Assets/Code/EntityReposition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityReposition : MonoBehaviour 
+{
+
+	void LateUpdate()
+    {
+        float z = transform.position.y;
+        z = z / 1000.0f;
+        transform.position = new Vector3(transform.position.x, transform.position.y, z);
+    }
+}
