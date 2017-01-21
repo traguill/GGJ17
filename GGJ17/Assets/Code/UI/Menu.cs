59fb76227de9c4a0484686f1e3d7ab9e490722c6
traguill <traguill1@gmail.com>
2017-01-21 16:02:38 +0100

    GameLoop + Menu + Credits

---
+++ GGJ17/Assets/Code/UI/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneManager.LoadScene("FinalComboAllTogether");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
