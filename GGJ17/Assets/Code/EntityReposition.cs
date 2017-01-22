2c4b28f0bc26e45472ae1fc5aeea7a2a33fdd007
J-Nunes <jordinunes96@gmail.com>
2017-01-22 03:21:19 +0100

    Game scene lighting improved

--- GGJ17/Assets/Code/EntityReposition.cs
+++ GGJ17/Assets/Code/EntityReposition.cs
@@ -7,7 +7,7 @@ public class EntityReposition : MonoBehaviour
 	void LateUpdate()
     {
         float z = transform.position.y;
-        z = z / 1000.0f;
+        z = z / 10000.0f;
         transform.position = new Vector3(transform.position.x, transform.position.y, z);
     }
 }
