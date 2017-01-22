84c55d767234092af8979fb36df0a4c2e8a57bec
J-Nunes <jordinunes96@gmail.com>
2017-01-22 03:03:04 +0100

    Illumination added

---
+++ GGJ17/Assets/Code/Candle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Candle : MonoBehaviour {
+
+    public float min_intensity;
+    public float max_intensity;
+
+    public float speed;
+    bool grow = true;
+
+    Light light;
+
+    void Awake()
+    {
+        light = GetComponent<Light>();
+        light.intensity = min_intensity;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if(grow)
+        {
+            light.intensity += speed * Time.deltaTime;
+            if (light.intensity >= max_intensity)
+                grow = false;
+        }
+        else
+        {
+            light.intensity -= speed * Time.deltaTime;
+            if (light.intensity <= min_intensity)
+                grow = true;
+        }
+    }
+}
