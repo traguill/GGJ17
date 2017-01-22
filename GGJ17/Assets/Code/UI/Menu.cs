44d00376b7f396194502183428ca9e8d787daaa6
J-Nunes <jordinunes96@gmail.com>
2017-01-22 13:19:35 +0100

    Menu Music added

--- GGJ17/Assets/Code/UI/Menu.cs
+++ GGJ17/Assets/Code/UI/Menu.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
+    public AudioClip music;
+    public AudioSource source;
+
+    void Start()
+    {
+        source.clip = music;
+        source.Play();
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Joystick1Button7))
@@ -12,11 +22,13 @@ public class Menu : MonoBehaviour
 
     public void Play()
     {
+        source.Stop();
         SceneManager.LoadScene("FinalComboAllTogether");
     }
 
     public void Credits()
     {
+        source.Stop();
         SceneManager.LoadScene("Credits");
     }
 }
